b9c74b899f9cf5a18e558e3bc9ae351d2030aeec
Mateus Moreira Santos <mmsantos@idejr.com.br>
2022-02-24 17:36:03 -0300

    Peças do museu agrupadas pela tag 'MuseumPiece' #1.2 DONE

--- Assets/Scripts/DogNavMesh.cs
+++ Assets/Scripts/DogNavMesh.cs
@@ -8,17 +8,21 @@ public class DogNavMesh : MonoBehaviour
 
     [SerializeField] private Transform movePositionTransform;
 
+    private GameObject[] poi; // Array of GameObjects to be the destinations for navMeshAgent
     private NavMeshAgent navMeshAgent;
 
     // Start is called before the first frame update
     private void Awake()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        poi = GameObject.FindGameObjectsWithTag("MuseumPiece");
     }
 
     // Update is called once per frame
     private void Update()
     {
-        navMeshAgent.destination = movePositionTransform.position;
+        if(Input.GetKeyDown(KeyCode.Space)) {
+            navMeshAgent.destination = movePositionTransform.position;
+        }
     }
 }
--- README.md
+++ README.md
@@ -4,10 +4,10 @@
 
 1. Cachorro seleciona objeto para interagir
 	1. [X] Objetos: Plano, cubos para interação e placeholder para o cachorro
-	2. [ ] Cada cubo é um ponto de interesse.
+	2. [X] Cada cubo é um ponto de interesse.
 	3. [ ] Escolha aleatória entre pontos de interesse
 2. Cachorro se move pelo mapa (https://www.youtube.com/watch?v=atCOd4o7tG4)
-	1. [ ] Pathfinding até ponto de interesse (!! Parte mais importante)
+	1. [X] Pathfinding até ponto de interesse (!! Parte mais importante)
 	2. [ ] Precisamos que o cachorro OU encoste no objeto OU esteja a uma distância que queremos
 3. Cachorro interage com objeto
 	1. [ ] A partir do ponto 2 do último tópico, precisamos setar uma propriedade no cubo
